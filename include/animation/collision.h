e23b1559066d9bdbbc36e6c325a08458f8c4ef71
hngwlog <hngwlog@gmail.com>
2025-04-03 11:31:56 +0700

    Add Collision class for AABB (Axis-Aligned Bounding Box) collision detection
    - Included debug rendering for collision visualization.

---
+++ include/animation/collision.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <SDL.h>
+#include "../init/graphics.h"
+
+enum COLLISION_STATE {
+
+    NONE = -1,
+    LEFT,
+    RIGHT,
+    TOP,
+    DOWN
+};
+
+class Collision {
+public:
+    Collision(Texture* body);
+    ~Collision();
+
+    int check(Collision* other, bool push = false);
+
+    SDL_FRect getRect() const;
+
+    void move(const float& x, const float& y);
+
+    void debugDraw();
+
+private:
+    Texture* body = nullptr;
+};
---
+++ src/animation/collision.cpp
@@ -0,0 +1,79 @@
+#include "../../include/animation/collision.h"
+#include "../../include/init/defs.h"
+
+Collision::Collision(Texture* body): body(body) {}
+
+Collision::~Collision() {
+
+    delete body;
+}
+
+int Collision::check(Collision* other, bool push) {
+
+    /** AABB Collision Detection (Axis-Aligned Bounding Box) */
+
+    SDL_FRect thisRect = getRect();
+    SDL_FRect otherRect = other->getRect();
+
+    float deltaX = (otherRect.x + otherRect.w * 0.5f) - (thisRect.x + thisRect.w * 0.5f);
+    float deltaY = (otherRect.y + otherRect.h * 0.5f) - (thisRect.y + thisRect.h * 0.5f);
+
+    float overlapX = (thisRect.w + otherRect.w) * 0.5f - abs(deltaX);
+    float overlapY = (thisRect.h + otherRect.h) * 0.5f - abs(deltaY);
+
+    if (overlapX >= 0 && overlapY >= 0) {
+
+        /** Prefer the less intersecting part */
+        if (overlapX < overlapY) {
+            if (deltaX > 0) {
+                /** this -> other */
+                if (push) other->move(overlapX, 0);
+                return COLLISION_STATE::RIGHT;
+            }
+            else {
+                /** other -> this */
+                if (push) other->move(- overlapX, 0);
+                return COLLISION_STATE::LEFT;
+            }
+        }
+        else {
+            if (deltaY > 0) {
+                /**
+                    this
+                    other
+                */
+                if (push) other->move(0, overlapY);
+                return COLLISION_STATE::DOWN;
+            }
+            else {
+                /**
+                    other
+                    this
+                */
+                if (push) other->move(0, - overlapY);
+                return COLLISION_STATE::TOP;
+            }
+        }
+    }
+
+    return COLLISION_STATE::NONE;
+}
+
+SDL_FRect Collision::getRect() const {
+
+    std::pair<int, int> pos = body->getPosition();
+    std::pair<int, int> size = body->getSize();
+    return {pos.first, pos.second, size.first, size.second};
+}
+
+void Collision::move(const float& x, const float& y) {
+
+    body->move({int(x), int(y)});
+}
+
+void Collision::debugDraw() {
+
+    SDL_FRect rect = getRect();
+    SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 255);
+    SDL_RenderDrawRectF(gRenderer, &rect);
+}
